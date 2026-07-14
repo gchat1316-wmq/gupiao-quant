82b0fb404b8620df6f54803143d6428b30e9adaf
wmq534 <wmq534@126.com>
2026-07-14 22:36:57 +0800

    test(xiebo-recent): add SchemaInitializerIT verifying 3 tables + 1 column

--- src/main/java/com/quant/config/XieboRecentAlertProperties.java
+++ src/main/java/com/quant/config/XieboRecentAlertProperties.java
@@ -2,9 +2,7 @@ package com.quant.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "xiebo-recent-alert")
 @Data
 public class XieboRecentAlertProperties {
